15c15d42c55334db9de5c93413e70df23ffb1e17
Viet Phi Nguyen <vietphi99dh@gmail.com>
2021-01-28 22:15:32 +0700

    styling products

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="gid-container">
-      <header>
-        <a href='/'>React Shopping Cart</a>
-      </header>
-      <main>
-        Product list
-      </main>
-      <footer>All rights is reserved</footer>
-    </div>
-  );
-}
+import data from './data.json';
+import Products from './components/Products';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+
+  render(){
+    return (
+      <div className='gid-container'>
+        <header>
+          <a href='/'>React Shopping Cart</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Products products={this.state.products} />
+            </div>
+            <div className='sidebar'>
+              Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>All rights is reserved</footer>
+      </div>
+    );
+  };
+};
 
 export default App;
